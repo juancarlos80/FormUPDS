8ac6ae42dc9d2989a70c349b102677f42827c3c5
Juan Carlos Crespo <juan.k.80@gmail.com>
2018-06-13 10:26:13 -0400

    Se añadio la opcion de enviar los datos al servidor y borrar los datos levantados tambien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
-
+import { HTTP } from '@ionic-native/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -30,6 +30,7 @@ import { ListadoPage } from '../pages/listado/listado';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/listado/listado.ts
+++ src/pages/listado/listado.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
 import { AlertController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { HTTP } from '@ionic-native/http';
+
+import { LoadingController } from 'ionic-angular'
 
 import { HomePage } from '../home/home';
+
+
 
 @Component({
   selector: 'page-home',
@@ -13,19 +17,106 @@ import { HomePage } from '../home/home';
 
 export class ListadoPage {
   encuestados: number;
-
-  storage: Storage;
   personas: any;
+  //http: HTTP;
 
+  constructor(public navCtrl: NavController,
+              public alerCtrl: AlertController,
+              public navParams: NavParams,
+              public http: HTTP,
+              public storage: Storage,
+              public loadingCtrl: LoadingController) {
 
-  constructor(public navCtrl: NavController, public alerCtrl: AlertController, public navParams: NavParams) {    
     this.personas = this.navParams.get('personas');
-    console.log(this.personas);
   }
 
 
   volver() {
     this.navCtrl.push(HomePage);
+  }
+
+  mandar(){
+
+    if( this.personas.length == 0 ){
+      this.mostrarError("No hay información para enviar");
+      return;
+    }
+
+    let loader = this.mostrar_loader();
+
+    //this.http.post('http://localhost/upds_form/send_data.php',
+    this.http.post('http://www.quarksocialcloud.com/apps_server/UPDS/Apps/FormularioMovil/send_data.php',
+      { users : JSON.stringify(this.personas) }, {}
+    ).then(data => {
+      let response;
+      try {
+        response = JSON.parse(data.data);
+      } catch( er_json ){
+        this.ocultar_loader(loader);
+        this.mostrarError( "Existe un problema con el envio del correo, intentelo nuevamente si el problema persiste contactese con su proovedor" );
+        return;
+      }
+
+      this.ocultar_loader(loader);
+      if( response.success == true ){
+        this.mostrarExito();
+      } else {
+        this.mostrarError( response.reason );
+      }
+    }).catch(error => {
+      this.ocultar_loader(loader);
+      this.mostrarError("No se pudo enviar los datos, compruebe su conexión e intentelo nuevamente");
+    });
+  }
+
+  borrar(){
+    let alert = this.alerCtrl.create({
+    title: 'Eliminar Datos',
+    message: 'Confirmas borrar permamentemente la informacion del equipo?',
+    buttons: [
+      {
+        text: 'No Borrar',
+        role: 'cancel'
+      },
+      {
+        text: 'Si Borrar',
+        handler: () => {
+          console.log('Ahora si borrar');
+          this.storage.set('encuestados', 0);
+          this.personas = [];
+        }
+      }
+    ]});
+    alert.present();
+  }
+
+  mostrarExito(){
+    let alert = this.alerCtrl.create({
+      title: 'Los datos se enviaron exitosamente!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+
+  mostrarError(mensaje: string){
+    let alert = this.alerCtrl.create({
+      title: mensaje,
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+
+  mostrar_loader() {
+    let loading = this.loadingCtrl.create({
+      content: 'Enviando...'
+    });
+
+    loading.present();
+    return loading;
+  }
+
+  ocultar_loader(loader){
+    loader.dismiss();
   }
 
 }
